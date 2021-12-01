5861c9ec7fdb82c0561b2ba1fcfc8e2c5c4ca830
Filip Tammergård <filip@tammergard.se>
2021-12-01 13:06:09 +0100

    feat: add solution for day 1

--- day01/index.ts
+++ day01/index.ts
@@ -1,1 +1,40 @@
+const day = "01";
 
+const decoder = new TextDecoder("utf-8");
+const input = await Deno.readFile(`day${day}/input.txt`);
+const data = decoder.decode(input);
+
+console.log(`
+Part one answer: ${partOne()}
+Part two answer: ${partTwo()}
+`);
+
+function partOne() {
+  const measurements = data
+    .split("\n")
+    .map((measurement) => parseInt(measurement, 10));
+
+  const count = measurements.reduce((acc, measurement, index) => {
+    if (measurements[index + 1] > measurement) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  return count;
+}
+
+function partTwo() {
+  const measurements = data
+    .split("\n")
+    .map((measurement) => parseInt(measurement, 10));
+
+  const count = measurements.reduce((acc, measurement, index) => {
+    if (measurements[index + 3] > measurement) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  return count;
+}
